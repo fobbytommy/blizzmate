c008e1a039a6f243e3a618e288508bd96b43e942
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-10-18 22:31:41 -0700

    Removed # hashtag in URL for partials.

--- client/assets/app.js
+++ client/assets/app.js
@@ -1,20 +1,23 @@
 var app = angular.module('app', ['ngRoute', 'ngCookies']);
 
-app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	$routeProvider
 		.when('/' ,{
-			templateUrl: 'views/cover.html',
+			templateUrl: '/views/cover.html',
 			controller: 'CoverController'
 		})
 		.when('/login', {
-			templateUrl: "views/login.html",
+			templateUrl: "/views/login.html",
 			controller: 'UserController'
 		})
 		.when('/home', {
-			templateUrl: "views/home.html",
+			templateUrl: "/views/home.html",
 			controller: 'HomeController'
 		})
 		.otherwise({
 			redirectTo: '/'
 		});
+
+	// use the HTML5 History API
+	$locationProvider.html5Mode(true);
 }]);
--- server.js
+++ server.js
@@ -21,10 +21,20 @@ app.use(express.static(path.join(root, 'client')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+
+
 // load mongoDB
 require('./server/config/mongoose');
 // traditional HTTP requests handler
 require('./server/config/routes')(app);
+
+// remove # hashtag fixer
+app.use('/*', function(req, res, next) {
+    // Just send the index.html for other files to support HTML5Mode
+    res.sendFile('/client/index.html', { root: __dirname });
+	// var indexFile = path.resolve(root, 'client/index.html');
+	// res.sendFile(indexFile);
+});
 
 var server = app.listen(port, function() {
 	console.log(`listening to port ${ port }`);
