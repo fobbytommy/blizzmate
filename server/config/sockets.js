2082497f5ac13a3965206366f574c6e9b65e5f20
Tommy Oh <tommyhyungjinoh@gmail.com>
2016-10-19 00:34:29 -0700

    initialized web socket connection with user count working.

--- server/config/sockets.js
+++ server/config/sockets.js
@@ -1,10 +1,20 @@
 module.exports = function(server) {
 	var io = require('socket.io').listen(server);
+	var userCount = 0;
 
 	io.sockets.on('connection', function(socket) {
 		console.log("SOCKET CONNECTION IS MADE!");
 		console.log(socket.id);
 
-	});
+		socket.on('user_online', function(data) {
+			userCount++;
+			io.emit('users_count', {userCount: userCount});
+		});
 
+		socket.on('user_offline', function(data) {
+			userCount--;
+			socket.broadcast.emit('users_count', {userCount: userCount});
+		});
+
+	});
 };
